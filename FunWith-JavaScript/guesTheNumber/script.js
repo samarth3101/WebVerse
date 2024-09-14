4f612cc9b738375fc3cb2469817828b0fde060a1
samarth <samarth.patil3101@gmail.com>
2024-09-14 22:21:04 +0530

    “Number guessing game with user-defined max limit.”
    This program lets the user set a maximum limit for the range of numbers and then attempts to guess the user’s chosen number. The guessing process continues until the correct number is found, providing feedback on whether the guess is too high or too low, making it an interactive and fun way to test the guessing algorithm.

---
+++ FunWith-JavaScript/guesTheNumber/script.js
@@ -0,0 +1,20 @@
+const max = prompt("Enter the max number");
+
+const random = Math.floor(Math.random() * max) + 1;
+
+let guess = prompt("Guess the number");
+
+while (true) {
+    if ( guess == "quit"){
+        console.log("User quit");
+        break;
+    }
+    if ( guess == random){
+        console.log("You are right!! random number was",random);
+        break;
+    } else if (guess < random){
+        guess = prompt("Your guess was small. Try again")
+    } else {
+        guess = prompt("Number was large. Try again");
+    }
+}
